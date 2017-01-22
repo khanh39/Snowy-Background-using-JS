621a54f5ad7e64ec58d4250f46891d7d59e53739
Hamza Khan <khanh39@mcmaster.ca>
2017-01-22 18:58:39 -0500

    added for loopto generate flakes

--- snow.js
+++ snow.js
@@ -10,5 +10,18 @@ window.onload = function() {
     canvas.width = W;
     canvas.height = H;
 
+    //generate the snowflakes and apply attributes
+    var mf = 100; //max flakes
+    var flakes = [];
 
+    //loop through the empty flakes and apply attributes
+    for (var i = 0; i < mf; i++)
+    {
+        flakes.push({
+            x: Math.random()*W,
+            y: Math.random()*H,
+            r: Math.random()*5+2 //min of 2px and max of 7px
+            d: Math.random() + 1 //density of the flake
+
+        })
 }
