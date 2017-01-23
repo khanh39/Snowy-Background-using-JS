c372990a4db7ddd0b91cc23d57a93fba9ce01be9
Hamza Khan <khanh39@mcmaster.ca>
2017-01-22 19:23:27 -0500

    added function to call 'drawFlakes' method and also fixed syntax errors

--- snow.js
+++ snow.js
@@ -1,67 +1,58 @@
-window.onload = function() {
+window.onload = function(){
+	//create canvas
+	var canvas = document.getElementById("sky");
+	var ctx = canvas.getContext("2d");
 
-    //get the canvas and context and store in vars
-    var canvas = document.getElementById("sky");
-    var ctx = canvas.getContext("2d");
+	//set canvas fullscreen
+	var W = window.innerWidth;
+	var H = window.innerHeight;
+	canvas.height = H;
+	canvas.width = W;
 
-    //set canvas dims to window height and width
-    var W = window.innerWidth;
-    var H = window.innerHeight;
-    canvas.width = W;
-    canvas.height = H;
+	//generate snowflakes and atts
+	var mf = 100; //max flakes
+	var flakes = [];
 
-    //generate the snowflakes and apply attributes
-    var mf = 100; //max flakes
-    var flakes = [];
 
-    //loop through the empty flakes and apply attributes
-    for (var i = 0; i < mf; i++)
-    {
-        flakes.push({
-            x: Math.random()*W,
-            y: Math.random()*H,
-            r: Math.random()*5+2 //min of 2px and max of 7px
-            d: Math.random() + 1 //density of the flake
-        })
-    }
+	//loop through empty flakes and apply atts
+for(var i = 0; i < mf; i++){
+		flakes.push({
+			x: Math.random()*W, //set width of flake to random nr between 0 and 1 * width of screen
+			y: Math.random()*H, //set height of flake to random nr between 0 and 1 * height of screen
+			r: Math.random()*5+2, //set radius between 2 and 5
+			d: Math.random() + 1
+		})
 
-    //draw flakes into canvas
-    function drawFlakes()
-    {
-        ctx.clearRect(0, 0, W, H);
-        ctx.fillstyle = "white";
-        ctx.beginPath();
-        for(var i = 0; i < mf; i++)
-        {
-            var f = flakes[i];
-            ctx.moveTo(f.x, f.y);
-            ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
-        }
-        ctx.fill();
-        moveFlakes();
+	}
 
-    }
 
-    //animate the flakes
-    var angle = 0;
+	//draw flakes
+	function drawFlakes(){
+		ctx.clearRect(0, 0, W, H);
+		ctx.fillStyle = "White";
+		ctx.beginPath();
+		for(var i = 0; i < mf; i++){
+			var f = flakes[i];
+			ctx.moveTo(f.x, f.y);
+			ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
+		}
+		ctx.fill();
+		moveFlakes();
+	}
+	var angle = 0;
+	//move flakes
+	function moveFlakes(){
+		angle += 0.01;
+		for(var i = 0; i < mf; i++){
+			var f = flakes[i];
+			f.y += Math.pow(f.d, 2) + 1;
+			f.x += Math.cos(angle)*2;
 
-    function moveFlakes(){
-      angle += 0.01;
-      for(var i = 0; i < mf; i++)
-      {
-          //store current fkale
-          var f = flakes[i];
+			if(f.y > H){
+				flakes[i] = {x: Math.random()*W, y: 0, r: f.r, d: f.d};
+			}
+		}
+	}
 
-          //update X and Y coordinates of each snowflake
-          f.y += Math.pow(f.d, 2) + 1;
-          f.x += Math.sin(angle) * 2;
-
-          //if the snowflake reaches the bottom, send a new one to to the top
-          if(f.y > H){
-              flakes[i] = {x: Math.random()*W, y: 0, r: f.r, d: f.d};
-
-        
-      }
-
-    }
+	setInterval(drawFlakes, 25);
 }
