0200e5703e44ff436683f063aa65ae1be9ff81fa
Hamza Khan <khanh39@mcmaster.ca>
2017-01-22 19:09:31 -0500

    added function to move flakes so they move downwards and side to side

--- snow.js
+++ snow.js
@@ -22,6 +22,46 @@ window.onload = function() {
             y: Math.random()*H,
             r: Math.random()*5+2 //min of 2px and max of 7px
             d: Math.random() + 1 //density of the flake
-
         })
+    }
+
+    //draw flakes into canvas
+    function drawFlakes()
+    {
+        ctx.clearRect(0, 0, W, H);
+        ctx.fillstyle = "white";
+        ctx.beginPath();
+        for(var i = 0; i < mf; i++)
+        {
+            var f = flakes[i];
+            ctx.moveTo(f.x, f.y);
+            ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
+        }
+        ctx.fill();
+        moveFlakes();
+
+    }
+
+    //animate the flakes
+    var angle = 0;
+
+    function moveFlakes(){
+      angle += 0.01;
+      for(var i = 0; i < mf; i++)
+      {
+          //store current fkale
+          var f = flakes[i];
+
+          //update X and Y coordinates of each snowflake
+          f.y += Math.pow(f.d, 2) + 1;
+          f.x += Math.sin(angle) * 2;
+
+          //if the snowflake reaches the bottom, send a new one to to the top
+          if(f.y > H){
+              flakes[i] = {x: Math.random()*W, y: 0, r: f.r, d: f.d};
+
+        
+      }
+
+    }
 }
